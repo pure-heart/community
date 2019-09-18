ff00406ef9f16a0a71a3c16af1525b04481af227
Jhly <13545175547@163.com>
2019-09-18 15:18:09 +0800

    完成文章发布功能

--- src/main/java/com/jhly/community/controller/publishController.java
+++ src/main/java/com/jhly/community/controller/publishController.java
@@ -39,6 +39,24 @@ public class publishController {
             @RequestParam("tag") String tag,
             HttpServletRequest request,
             Model model){
+
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+
+        if (title ==null|| title ==""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+        if (description ==null|| description ==""){
+            model.addAttribute("error","问题补充不能为空");
+            return "publish";
+        }
+        if (tag ==null|| tag ==""){
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+
         User user =null;
         Cookie[] cookies = request.getCookies();
         if (cookies!=null) {
