f211479f5bdae27c211f2f73e19bd23ea579976d
Jhly <13545175547@163.com>
2019-10-30 22:52:32 +0800

    实现问题回复-不刷新页面登陆

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -2,19 +2,30 @@ function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
     $.ajax({
-        type:"post",
-        url:"/comment",
-        contentType:"application/json",
-        data:JSON.stringify({
-            "parentId":questionId,
-            "content":content,
-            "type":1
+        type: "post",
+        url: "/comment",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
         }),
-        success:function (response) {
-            if (response.code == 200){
+        success: function (response) {
+            if (response.code == 200) {
                 $("#comment_section").hide();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=247b0787132273aa047f&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable",true);
+
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
         },
-        dataType:"json"
+        dataType: "json"
     });
 }
